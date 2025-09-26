139f01f3135fdbd861c600c33a046d601e708d20
tk1981215 <truptikhot44@gmail.com>
2025-09-26 22:29:19 +0530

    Create Conditional statements.c

---
+++ Conditional statements.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int age = 16;
+    if (age >= 18) {
+        printf("You are eligible to vote.\n");
+    } else {
+        printf("You are NOT eligible to vote.\n");
+    }
+    return 0;
+}
