b12d4a32ca5a6f256fffd126f4375d2db9c00faa
tk1981215 <truptikhot44@gmail.com>
2025-10-11 22:43:39 +0530

    Create Boolean datatype.c

---
+++ Boolean datatype.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool isRaining = true;
+    if (isRaining)
+        printf("Take an umbrella!\n");
+    else
+        printf("Enjoy the sunshine!\n");
+    return 0;
+}
