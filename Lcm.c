30e7c159d5f3f9cc922cfb600178be877d6c62cc
tk1981215 <truptikhot44@gmail.com>
2025-10-18 19:06:17 +0530

    Create Lcm.c

---
+++ Lcm.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, max;
+
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Find the greater number
+    max = (num1 > num2) ? num1 : num2;
+
+    while (1) {
+        if (max % num1 == 0 && max % num2 == 0) {
+            printf("The LCM of %d and %d is %d.
+", num1, num2, max);
+            break;
+        }
+        ++max;
+    }
+    return 0;
+}
