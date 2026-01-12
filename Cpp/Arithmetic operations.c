7e6284f976acd3ca1dc206fcdaf803629c12a77a
tk1981215 <truptikhot44@gmail.com>
2026-01-12 21:41:06 +0530

    Create Arithmetic operations.c

---
+++ Cpp/Arithmetic operations.c
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1;
+int num2;
+cout<<"Enter Num1: "<<endl;
+cin>>num1;
+cout<<"Enter Num2: "<<endl;
+cin>>num2;
+cout<<"Sum: "<<num1+num2<<endl;
+cout<<"Difference: "<<num1-num2<<endl;
+cout<<"Product: "<<num1*num2<<endl;
+return 0;
+}
