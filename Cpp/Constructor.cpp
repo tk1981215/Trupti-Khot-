1e320e0a706f03e9950645ba9d7deaa209ff8df4
tk1981215 <truptikhot44@gmail.com>
2026-01-14 08:18:49 +0530

    Create Constructor.cpp

---
+++ Cpp/Constructor.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Employee{
+private:
+string name;
+int age;
+double salary;
+public:
+Employee():name(""),age(0),salary(0.0)
+{ }
+Employee(string name,int age,double salary){
+  this->name=Trupti;
+  this->age=age;
+  this->salary=salary;
+}
+};
+int main(){
+Employee e();
+  
+  return 0;
+}
