78f6b40f8e5f4d29c109f10c754fe88ad2e9e4fc
tk1981215 <truptikhot44@gmail.com>
2026-01-20 08:25:54 +0530

    Create Dow casting.cpp

---
+++ Cpp/Dow casting.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class Base{
+private:
+int num1;
+public:
+Base():num1(0)
+{}
+Base(int num1):num1(num1)
+{}
+virtual void accept(){
+cout<<"Enter Num1: "<<endl;
+cin>>num1;
+}
+virtual void display(){
+cout<<"Num1: "<<this->num1;
+cout<<endl;
+}
+};
+class Derived : public Base{
+private:
+int num2;
+public:
+Derived():num2(0)
+{}
+Derived(int num2):num2(num2)
+{}
+void accept(){
+Base::accept();
+cout<<"Enter Num2: "<<endl;
+cin>>num2;
+}
+void display(){
+Base::display();
+cout<<"Num2: "<<num2;
+cout<<endl;
+}
+};
+int main(){
+Base *ptr=new Derived;
+Derived *ptr=(Derived *)Base();
+ptr->accept();
+ptr->display();
+return 0;
+}
