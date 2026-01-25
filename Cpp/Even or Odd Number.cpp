bc78ea9bfb5d6c7a7ae2828d87cb94c57c7740f6
tk1981215 <truptikhot44@gmail.com>
2026-01-25 18:23:21 +0530

    Create Even or Odd Number.cpp

---
+++ Cpp/Even or Odd Number.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int num;
+cin>>num;
+if(num%2==0){
+cout<<"Number is even"<<endl;
+}
+else{
+cout<<"Number is odd"<<endl;
+}
+return 0;
+}
