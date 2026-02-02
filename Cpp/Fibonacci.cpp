ba15d6a2409002b1b587246caff48f6c8f6fc8a4
tk1981215 <truptikhot44@gmail.com>
2026-02-02 20:43:44 +0530

    Create Fibonacci.cpp

---
+++ Cpp/Fibonacci.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fibonacci(){
+int num;
+int fibonacci(n)=fibonacci(n-1)+fibonacci(n-2)!
+return fibonacci;
+}
+int main(){
+int num;
+cin>>num;
+fibonacci();
+return 0;
+}
