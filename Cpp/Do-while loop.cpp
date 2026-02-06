9efe34c71f8f6ddb99f91f7b54235589c10006c5
tk1981215 <truptikhot44@gmail.com>
2026-02-07 00:01:18 +0530

    Create Do-while loop.cpp

---
+++ Cpp/Do-while loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    do {
+        cout << i << " ";
+        i++;
+    } while (i <= 5);
+    cout << endl;  // Output: 1 2 3 4 5
+    return 0;
+}
