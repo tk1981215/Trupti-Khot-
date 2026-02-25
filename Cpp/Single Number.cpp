4466490e5d36827d93e783f87252b39a038daa7a
tk1981215 <truptikhot44@gmail.com>
2026-02-25 21:37:48 +0530

    Create Single Number.cpp

---
+++ Cpp/Single Number.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+int singleNumber(vector<int>& nums){
+  int answer=0;
+  for(int i=0;i<nums.size();i++){
+    answer ^= nums[i];
+  }
+  return answer;
+}
+};
