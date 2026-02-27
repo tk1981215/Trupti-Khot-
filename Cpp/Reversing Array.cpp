e1dfe6941a0e3758e0d609297d39d061c24efc30
tk1981215 <truptikhot44@gmail.com>
2026-02-27 21:12:42 +0530

    Create Reversing Array.cpp

---
+++ Cpp/Reversing Array.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int arr[10];
+cout<<"Elements Before reversing"<<endl;
+for(int i=0;i<=n;i++){
+cout<<"Element: "<<endl;
+}
+cout<<"Elements after reversing "<<endl;
+for(int i=n-1;i>=0;i--){
+cout<<"Element: "<<endl;
+}
+return 0;
+}
