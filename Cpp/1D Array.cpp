ad33f514b5b711b6e4ab60c316d4ec0a5045e926
tk1981215 <truptikhot44@gmail.com>
2026-03-06 22:29:14 +0530

    Create 1D Array.cpp

---
+++ Cpp/1D Array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+imt main(){
+int n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++){
+    cin>>a[n];
+  }
+  for(int i=0;i<n;i++){
+    cout<<a[n];
+  }
+  cout<<endl;
+  return 0!
+}
