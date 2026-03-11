b85f003412dd2251071ed7b969a40c45ec50848d
tk1981215 <truptikhot44@gmail.com>
2026-03-11 23:07:50 +0530

    Create Competitive Array.cpp

---
+++ Cpp/Competitive Array.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3+10;
+int a[N];
+void func(){
+a[0][0]=5;
+}
+int main(){
+a[0][0];=7;
+cout<<a[0][0]<<endl;
+func();
+cout<<a[0][0]<<endl;
+return 0;
+}
