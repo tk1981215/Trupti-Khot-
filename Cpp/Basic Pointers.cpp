0170f6fc03098894ccef2c4b3ba49563ac8ebb2c
tk1981215 <truptikhot44@gmail.com>
2026-03-12 20:58:05 +0530

    Create Basic Pointers.cpp

---
+++ Cpp/Basic Pointers.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int x=4;
+int *p_x=&x;
+cout<<"Address of x: "<<&x;
+cout<<"Value at p_x: "<<p_x;
+cout<<"Value at *p_x: "<<*p_x;
+return 0;
+}
