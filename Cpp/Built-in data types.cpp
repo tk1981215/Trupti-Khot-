c65b199cde4b893ce87c3bc9fcd377f54906e17e
tk1981215 <truptikhot44@gmail.com>
2026-03-16 22:30:15 +0530

    Create Built-in data types.cpp

---
+++ Cpp/Built-in data types.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a=4;
+float pi=3.14;
+char c='u';
+bool is_true=true;
+cout<<"The value of a is "<<a<<"\nThe value of b is "<<b;
+cout<<"The value of c is "<<c;
+return 0;
+}
