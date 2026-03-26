655cf00dda908549019cb49c244a99d48f040509
tk1981215 <truptikhot44@gmail.com>
2026-03-26 21:32:49 +0530

    Add arithmetic operators demonstration in C++

---
+++ Cpp/Arithmetic Operators.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a,b;
+cin>>a>>b;
+  cout<<"Sum: "<<a+b;
+  cout<<"Substraction: "<<a-b;
+  cout<<"Multiplication: "<<a*b;
+  cout<<"Division: "<<a/b;
+  cout<<"Modulus: "<<a%b;
+  cout<<"++a: "<<++a;
+  cout<<"a++: "<<a++;
+  return 0;
+}
