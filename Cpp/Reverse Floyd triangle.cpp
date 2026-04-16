1c77260e229fe63d0bcde100f60cea8c2c7cc4ec
tk1981215 <truptikhot44@gmail.com>
2026-04-16 21:46:15 +0530

    Add Reverse Floyd triangle implementation in C++

---
+++ Cpp/Reverse Floyd triangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int num = 1;
+
+    for(int i = n; i >= 1; i--) {
+        for(int j = 1; j <= i; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
