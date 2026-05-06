cb4e9a9cb87d6f30a8c70cf6fefdec408884ad6f
tk1981215 <truptikhot44@gmail.com>
2026-05-06 01:00:17 -0700

    Create calculate GCD.cpp

---
+++ Cpp/calculate GCD.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"Enter two numbers: ";
+  cin>>a>>b;
+  while(b != 0){
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+  cout<<"GCD is : "<<a<<endl;
+  return 0;
+}
