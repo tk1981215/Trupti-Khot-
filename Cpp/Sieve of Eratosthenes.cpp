0d4f451c147de3a9295a665b4856e7e9abd87067
tk1981215 <truptikhot44@gmail.com>
2026-05-09 22:48:19 +0530

    Create Sieve of Eratosthenes.cpp

---
+++ Cpp/Sieve of Eratosthenes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sieveOfEratosthenes(int n) {
+    vector<bool> isPrime(n + 1, true);
+
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    cout << "Prime numbers up to " << n << " are:\n";
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            cout << i << " ";
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Enter limit: ";
+    cin >> n;
+
+    sieveOfEratosthenes(n);
+
+    return 0;
+}
