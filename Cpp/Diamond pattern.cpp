ed4ba6315a1be7fea43ed5e18c6948499c713abb
tk1981215 <truptikhot44@gmail.com>
2026-05-15 21:51:29 +0530

    Create Diamond pattern.cpp

---
+++ Cpp/Diamond pattern.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    // Upper part
+    for (int i = 1; i <= n; i++) {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower part
+    for (int i = n - 1; i >= 1; i--) {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
