5e018f520be54c05aab283dd6d8ff04a55b34a75
tk1981215 <truptikhot44@gmail.com>
2026-05-19 18:20:42 -0700

    Create Armstrong Number.cpp

---
+++ Cpp/Armstrong Number.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int num, originalNum, remainder, result = 0, digits = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    originalNum = num;
+
+    // Count number of digits
+    while (originalNum != 0) {
+        originalNum /= 10;
+        digits++;
+    }
+
+    originalNum = num;
+
+    // Calculate Armstrong sum
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += pow(remainder, digits);
+        originalNum /= 10;
+    }
+
+    // Check Armstrong number
+    if (result == num)
+        cout << num << " is an Armstrong number.";
+    else
+        cout << num << " is not an Armstrong number.";
+
+    return 0;
+}
