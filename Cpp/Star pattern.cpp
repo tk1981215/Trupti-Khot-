f43084a1b15abec6b03a7e7c0d990ad2337f3295
tk1981215 <truptikhot44@gmail.com>
2026-05-30 12:33:19 +0530

    Create Star pattern.cpp

---
+++ Cpp/Star pattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
