018076ede1794564ef48f3f739d51acac0d21001
tk1981215 <truptikhot44@gmail.com>
2026-06-22 23:04:43 +0530

    Create Diamond Star Pattern.cpp

---
+++ Cpp/Diamond Star Pattern.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    // Upper half
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++)
+            cout << " ";
+
+        for (int j = 1; j <= 2 * i - 1; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    // Lower half
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++)
+            cout << " ";
+
+        for (int j = 1; j <= 2 * i - 1; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
+}
