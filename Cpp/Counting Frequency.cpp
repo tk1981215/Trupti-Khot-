360ce677f5068185bfc7d3b5a27b04f1960186f5
tk1981215 <truptikhot44@gmail.com>
2026-06-25 21:27:18 +0530

    Create Counting Frequency.cpp

---
+++ Cpp/Counting Frequency.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    int freq[256] = {0};
+
+    for (int i = 0; i < str.length(); i++) {
+        freq[(int)str[i]]++;
+    }
+
+    cout << "\nCharacter Frequencies:\n";
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > 0) {
+            cout << (char)i << " : " << freq[i] << endl;
+        }
+    }
+
+    return 0;
+}
