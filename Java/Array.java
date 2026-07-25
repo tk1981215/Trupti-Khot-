462364319f0cd8438fea92af83c0019f01d0571e
tk1981215 <truptikhot44@gmail.com>
2026-07-25 09:33:17 +0530

    Create Array.java

---
+++ Java/Array.java
@@ -0,0 +1,8 @@
+class Program{
+  public static void main(String[] args){
+    int[] nums = {10,20,30,40,50,60};
+    for(int i=0;i<nums.length;i++){
+      System.out.println(nums[i]);
+    }
+  }
+}
