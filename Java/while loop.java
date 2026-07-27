175e461fe7cf7b00e623a8b059effe3114c5872f
tk1981215 <truptikhot44@gmail.com>
2026-07-27 20:07:33 +0530

    Create while loop.java

---
+++ Java/while loop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main{
+  public static void main(String[] args){
+    int sum =0;
+    int i=0;
+    while(i<5){
+      sum += i;
+      i++;
+      System.out.println("Sum : "+sum);
+    }
+    
+  }
+}
