60dabd985df8784254872bcf977a5eb45038975b
tk1981215 <truptikhot44@gmail.com>
2026-07-30 20:31:28 +0530

    Create Printing Pattern.java

---
+++ Java/Printing Pattern.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Program{
+  public static void main(String[] args){
+    for(int i=1;i<5;i++){
+      for(int j=i;j<5;j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
