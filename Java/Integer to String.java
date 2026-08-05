cf82d3444d4c163992bd0efeb56da5c8bc4aedbd
tk1981215 <truptikhot44@gmail.com>
2026-08-05 22:24:00 +0530

    Create Integer to String.java

---
+++ Java/Integer to String.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Program{
+  public static void main(String[] args){
+    String s = "100";
+    int a  = Integer.parseInt(s);
+    System.out.println(a);
+  }
+}
