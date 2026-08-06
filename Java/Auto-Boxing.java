18962dd4dc70f053aa48e4316b22858112cf06dd
tk1981215 <truptikhot44@gmail.com>
2026-08-06 22:09:41 +0530

    Create Auto-Boxing.java

---
+++ Java/Auto-Boxing.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Program{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    Integer b = new Integer(a);
+    System.out.println("b : " + b);
+  }
+}
