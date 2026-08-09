c225a76398edaa43e4b427853fb696c70b8af686
tk1981215 <truptikhot44@gmail.com>
2026-08-09 20:04:50 +0530

    Add Max_Value.java to display integer limits

---
+++ Java/Max_Value.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Program{
+  public static void main(String[] args){
+    System.out.println("Max_Value : " + Integer.MAX_VALUE);
+    System.out.println("Min_Value : " + Integer.MIN_VALUE);
+  }
+}
