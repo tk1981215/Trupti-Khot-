b1524886147b088dc40f23fb3d7d0d9a0ce0df10
tk1981215 <truptikhot44@gmail.com>
2026-08-12 23:12:37 +0530

    Create Box.java

---
+++ Java/Box.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Box{
+  private double height;
+  private double width;
+  private double breadth;
+
+  public Box(double h,double w,double b){
+    this.height=h;
+    this.width=w;
+    this.breadth=b;
+  }
+}
