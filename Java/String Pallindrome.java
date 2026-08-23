62133925bcc741465abab1c033783e5e6bbe8689
tk1981215 <truptikhot44@gmail.com>
2026-08-23 14:59:53 +0530

    Create String Pallindrome.java

---
+++ Java/String Pallindrome.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+  public static String isLargestSmallest(String s,int k){
+    String smallest = s.substring(0,k);
+    String largest = s.substring(0,k);
+    for(int i=1;i<s.length()-k;i++){
+      String current = s.substring(i,i+k);
+      if(current.compareTo(smallest)<0){
+        smallest = current;
+      }
+      if(current.compareTo(largest) >0){
+        largest = current;
+      }
+    }
+    return smallest + "/n" + largest;
+  }
+}
