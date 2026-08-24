965283f3002faeba5de7a675c18a8eeb27ff1aac
tk1981215 <truptikhot44@gmail.com>
2026-08-24 22:10:37 +0530

    Create Command Line Argument.java

---
+++ Java/Command Line Argument.java
@@ -0,0 +1,10 @@
+import java.util.*:
+public class Main{
+  public static void main(String[] args){
+    String arg = args;
+    int[] num = sc.parseInt(arg);
+    for(int i=0;i<num.length;i++){
+      System.out.println(num[i]);
+    }
+  }
+}
