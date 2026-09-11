f87ae7fd3e24bf952f7ce870c0fcaf77833b5bc0
tk1981215 <truptikhot44@gmail.com>
2026-09-11 22:37:33 +0530

    Add ArrayExample class to demonstrate array usage

---
+++ Java/Arr.java
@@ -0,0 +1,12 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+
+        // Creating an array
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Printing array elements
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
