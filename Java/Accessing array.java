42717575fc011ee9b8d83a5b9eff6454567235e4
tk1981215 <truptikhot44@gmail.com>
2026-09-12 23:08:56 +0530

    Add array access example in Accessing array.java

---
+++ Java/Accessing array.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Creating an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Accessing array elements
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+    }
+}
