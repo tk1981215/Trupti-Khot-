c144f2c6561aa3fa35fc792c02f79e8cc8d29476
tk1981215 <truptikhot44@gmail.com>
2026-09-16 22:36:53 +0530

    Create Static variable.java

---
+++ Java/Static variable.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Student {
+    private int id;
+    private static String dept;
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        s1.id = 1;
+        s1.dept = "AIML";
+    }
+}
