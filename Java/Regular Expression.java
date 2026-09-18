02599cf1992086a36a1b320bd1cc2f3cb9f9d867
tk1981215 <truptikhot44@gmail.com>
2026-09-18 19:20:29 +0530

    Create Regular Expression.java

---
+++ Java/Regular Expression.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(Syystem.in);
+    String s = sc.hasNextLine();
+    s = s.trim().split("A-Za-z0-9");
+    sc.close();
+  }
+}
